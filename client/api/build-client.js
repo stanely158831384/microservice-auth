d7062e516704b722d9d35d1459870e9676820d4f
Junjie Zhang <junjiezhang@Junjies-MacBook-Pro.local>
2022-06-05 23:59:33 -0400

    tuning develop-dev3

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 const custom_build = ({req}) =>{
     if(typeof window === 'undefined'){
 
-        var baseURL = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+        // var baseURL = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
 
-        if(process.env.NODE_ENV != 'development'){
-            baseURL = 'http://www.racoonrepublic.xyz/';
-        }
+        // if(process.env.NODE_ENV != 'development'){
+        //     baseURL = 'http://www.racoonrepublic.xyz/';
+        // }
 
+
+        var baseURL = 'http://www.racoonrepublic.xyz/';
         return axios.create({
             baseURL,
             headers: req.headers
