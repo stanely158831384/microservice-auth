04b03cf18e6010c1af1bed985f984b230fa58b23
Junjie Zhang <junjiezhang@Junjies-MacBook-Pro.local>
2022-08-10 23:05:52 -0400

    mount up to new server

---
+++ client/api/build-client 2.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const custom_build = ({ req }) => {
+    if (typeof window === 'undefined') {
+        var baseURL = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+
+        return axios.create({
+            baseURL,
+            headers: req.headers
+        });
+    } else {
+        return axios.create({
+            baseURL: '/'
+        })
+    }
+}
+
+export default custom_build;
