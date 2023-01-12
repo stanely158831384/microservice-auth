a6dbfd8c6b7f8e4b91f0693bc80410ccaf7624d2
Junjie Zhang <junjiezhang@junjies-mbp.home>
2023-01-11 21:37:39 -0500

    adding multiprocess and multithread

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,9 +3,9 @@ import axios from "axios";
 //this is going to tell us where are we, because this code could be run by server or browser.
 const custom_build = ({ req }) => {
   if (typeof window === "undefined") {
-    // var baseURL =
-    //   "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
-    var baseURL = "http://www.racoonrepublic.ca/";
+    var baseURL =
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+    // var baseURL = "http://www.racoonrepublic.ca/";
     return axios.create({
       baseURL,
       headers: req.headers,
