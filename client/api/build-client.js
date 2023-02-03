7615514915159a96275be891306f1d286fd9f871
Junjie Zhang <junjiezhang@junjies-mbp.home>
2023-02-03 17:24:27 -0500

    add github deploy documents

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -7,6 +7,7 @@ const custom_build = ({ req }) => {
     //   "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
     var baseURL = "http://www.racoonrepublic.ca/";
     var asb = "asdf";
+
     return axios.create({
       baseURL,
       headers: req.headers,
