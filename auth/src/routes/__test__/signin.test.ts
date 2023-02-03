3fc1b78fca7fb51b33d71a9d08e20684a42fdf6c
Junjie Zhang <junjiezhang@junjies-mbp.home>
2023-02-02 22:54:38 -0500

    fix order test problem

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,41 +1,46 @@
 import request from "supertest";
-import { app } from '../../app';
+import { app } from "../../app";
 
+it("fails when an incorrect that does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "stans1@stans1.com",
+      password: "stan12345",
+      address: "104 sadlee",
+      username: "stans1",
+      phone: "6476470000",
+    })
+    .expect(201);
 
-it('fails when an incorrect that does not exist is supplied', async()=>{
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201)
-    
-    await request(app)
-        .post('/api/users/signin')
-        .send({
-            email: 'test@test.com',
-            password: 'asdfsadfsdf'
-        })
-        .expect(400);
-})
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "asdfsadfsdf",
+    })
+    .expect(400);
+});
 
-it('responds with a cookie when given valid credentials', async()=>{
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201)
-    
-    const response = await request(app)
-        .post('/api/users/signin')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(200);
+it("responds with a cookie when given valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "stans1@stans1.com",
+      password: "stan12345",
+      address: "104 sadlee",
+      username: "stans1",
+      phone: "6476470000",
+    })
+    .expect(201);
 
-    expect(response.get('Set-Cookie')).toBeDefined();
-})
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "stans1@stans1.com",
+      password: "stan12345",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
