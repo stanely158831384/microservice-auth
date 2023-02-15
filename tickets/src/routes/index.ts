f6ee167a8eec287cdc824d50ef10728ddded6752
Junjie Zhang <junjiezhang@junjies-mbp.home>
2023-02-15 18:49:40 -0500

    bug fixing 18

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -1,14 +1,14 @@
-import express, {Request, Response} from 'express';
-import { Ticket } from '../models/tickets'
+import express, { Request, Response } from "express";
+import { Ticket } from "../models/tickets";
 
 const router = express.Router();
 
-router.get('/api/tickets', async(req: Request, res: Response)=>{
-    const tickets = await Ticket.find({
-        orderId: undefined,
-    });
+router.get("/api/tickets", async (req: Request, res: Response) => {
+  const tickets = await Ticket.find({
+    orderId: undefined,
+  });
 
-    res.send(tickets);
+  res.send(tickets);
 });
 
-export {router as indexTicketRouter };
+export { router as indexTicketRouter };
