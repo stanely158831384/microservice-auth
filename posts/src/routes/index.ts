d9e9563bf0ed1fa032f39edec3a44fac517dc5db
Junjie Zhang <junjiezhang@junjies-mbp.home>
2023-02-14 22:29:44 -0500

    bug fixing 1

--- posts/src/routes/index.ts
+++ posts/src/routes/index.ts
@@ -1,12 +1,16 @@
-import express, {Request, Response} from 'express'; 
-import { Post } from '../models/post';
+import express, { Request, Response } from "express";
+import { Post } from "../models/post";
+import { validateRequest, BadRequestError } from "@racoonrepublic/common";
 
 const router = express.Router();
 
-router.get('/api/posts', async(req: Request, res: Response)=>{
-    const post = await Post.find({});
+router.get("/api/posts", async (req: Request, res: Response) => {
+  const post = await Post.find({});
+  if (!post) {
+    console.log("post", post);
+    throw new BadRequestError("post find error");
+  }
+  res.status(200).send(post);
+});
 
-    res.send(post);
-})
-
-export{router as indexPostRouter}
+export { router as indexPostRouter };
