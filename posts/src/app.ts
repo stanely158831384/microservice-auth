a82c876f5f66f9577e390d3a72bd2ff09eee3069
Junjie Zhang <junjiezhang@junjies-mbp.home>
2023-02-15 00:26:43 -0500

    bug fixing 8

--- posts/src/app.ts
+++ posts/src/app.ts
@@ -26,11 +26,11 @@ app.use(
 
 app.use(currentUser);
 
-app.use(deletePostRouter);
-app.use(indexPostRouter);
-app.use(newPostRouter);
-app.use(showPostRouter);
-app.use(updatePostRouter);
+// app.use(deletePostRouter);
+// app.use(indexPostRouter);
+// app.use(newPostRouter);
+// app.use(showPostRouter);
+// app.use(updatePostRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
